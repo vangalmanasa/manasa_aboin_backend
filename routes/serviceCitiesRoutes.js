298a05ce129fb26df4bf8a4746eadfd43e1dbf50
ParamJoga07 <parmeshwar5jan@gmail.com>
2025-05-05 11:36:02 +0530

    AUF-9 : updating location files

--- routes/serviceCitiesRoutes.js
+++ routes/serviceCitiesRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/service-cities", (req, res) => {
-  const cities = ["Hyderabad", "Mumbai", "Delhi", "Bangalore", "Chennai"];
+  const cities = ["Hyderabad", "Vijayawada"];
   res.json({ cities });
 });
 
